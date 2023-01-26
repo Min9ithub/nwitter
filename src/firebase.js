94e2c9611873367ae6c86f0ab41bd62eff9dc9e4
Nam Min Ki <nam960216@gmail.com>
2023-01-26 13:30:24 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCW436t-q-qz--6i2QHcBThHRUbbJBbN4A",
+  authDomain: "nwitter-30e54.firebaseapp.com",
+  projectId: "nwitter-30e54",
+  storageBucket: "nwitter-30e54.appspot.com",
+  messagingSenderId: "565545993102",
+  appId: "1:565545993102:web:8eec9c8ca1dd25ed865adf",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
