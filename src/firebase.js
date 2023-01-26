2fbb4f8a2414edbd0d7a4e6cf5ae0258f0eaf1c0
Nam Min Ki <nam960216@gmail.com>
2023-01-26 17:31:16 +0900

    1.2 Router Setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCW436t-q-qz--6i2QHcBThHRUbbJBbN4A",
-  authDomain: "nwitter-30e54.firebaseapp.com",
-  projectId: "nwitter-30e54",
-  storageBucket: "nwitter-30e54.appspot.com",
-  messagingSenderId: "565545993102",
-  appId: "1:565545993102:web:8eec9c8ca1dd25ed865adf",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
