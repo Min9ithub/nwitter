aa279b8da092f9d4d8a7d91609d8a4bf3de5e700
Nam Min Ki <nam960216@gmail.com>
2023-01-27 13:30:31 +0900

    2.1 Login Form part One

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
+import { authSerivce } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authSerivce.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />{" "}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
