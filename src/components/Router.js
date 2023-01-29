f8e0aba9dbc2fc5d985c267193313ffd60b45059
Nam Min Ki <nam960216@gmail.com>
2023-01-29 13:24:20 +0900

    2.6 Log Out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route path="/" element={<Home />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
           </>
         ) : (
           <Route path="/" element={<Auth />}></Route>
